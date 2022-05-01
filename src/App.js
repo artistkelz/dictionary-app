b1e0443613915b111fcdbf4f0a8e691decf98c9a
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-30 17:42:09 -0700

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import book from "./book.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={book} className="App-logo img-fluid" alt="logo" />
-        <h1>Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={book} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          Coded by Kelsey Murray hosted on Netifly and open sourced on Github
+        </footer>
+      </div>
     </div>
   );
 }
