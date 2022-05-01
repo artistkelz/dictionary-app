f3937450dfece38c41ed2f826a10ad665c0e09a9
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-30 22:17:18 -0700

    Added in meanings with loops from API

---
+++ src/Results.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+  console.log(props.data);
+  if (props.data) {
+    return (
+      <div className="Results">
+        <h2>{props.data.word}</h2>
+        {props.data.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
