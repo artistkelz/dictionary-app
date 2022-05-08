efa52fe160c85423114e2f9eb01431c1b3d0ebe8
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-05-07 23:09:18 -0700

    Styling meaning and phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -8,20 +9,18 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong>
-              {definition.definition}
-              <br />
+            <div className="definition">{definition.definition}</div>
+            <br />
+            <div className="example">
               {definition.example && (
                 <>
                   <br />
-                  <strong>Example:</strong>
                   <em>{definition.example}</em>
                   <br />
                 </>
               )}
-              <br />
-            </p>
+            </div>
+            <br />
           </div>
         );
       })}
